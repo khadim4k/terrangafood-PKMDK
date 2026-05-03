006b0ca53741506ff5d33d225426e3d1a9e29bcc
khadija <topsokhna196@gmail.com>
2026-05-04 01:14:02 +0200

    Mes modifications sur Header.js

--- web/components/Header.js
+++ web/components/Header.js
@@ -4,13 +4,12 @@ export default function Header() {
   return (
     <header className="header">
       <div className="header-content">
-           🍛 Terranga<span>Food</span>
+        <Link href="/" className="header-logo">
+          🍛 Terranga<span>Food</span>
         </Link>
         <nav className="header-nav">
           <Link href="/">Restaurants</Link>
-          <Link href="/mes-commandes">
-            Mes commandes
-          </Link>
+          <Link href="/mes-commandes">Mes commandes</Link>
         </nav>
       </div>
     </header>
